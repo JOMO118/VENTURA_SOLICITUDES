fabaf0904f9990ea0951b9147a14cb3c1c57fdad
JOMO118 <jomo0118@gmail.com>
2023-06-28 15:04:16 -0500

    licencia y permisos

--- src/app/user-licencia/user-licencia.page.ts
+++ src/app/user-licencia/user-licencia.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -9,6 +10,10 @@ import { AlertController } from '@ionic/angular';
 })
 export class UserLicenciaPage implements OnInit {
   localname = localStorage.getItem('nombre')
+
+  documentos: File[] = [];
+  miniaturas: string[] = [];
+
 
   motivos = [
     { constancia: false, descripcion: "Permiso dia de cumpleaños" },
@@ -23,7 +28,7 @@ export class UserLicenciaPage implements OnInit {
 
   motivoForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, public alertController: AlertController) {
+  constructor(private formBuilder: FormBuilder, public alertController: AlertController, private sanitizer: DomSanitizer) {
     this.motivoForm = this.formBuilder.group({
       'motivo': ['', Validators.required],
       'semestre': '',
@@ -39,6 +44,7 @@ export class UserLicenciaPage implements OnInit {
       this.motivoForm.get('fechaFin')?.disable()
     }
   }
+
 
   onMotivoChange() {
     const motivoSeleccionado = this.motivoForm.value.motivo.descripcion;
@@ -61,11 +67,7 @@ export class UserLicenciaPage implements OnInit {
     this.motivoForm.get('fechaFin')?.enable()
   }
 
-  isFechaDisabled() {
-    const motivoSeleccionado = this.motivoForm.value.motivo.descripcion;
-    const semestreSeleccionado = this.motivoForm.value.semestre;
-    return motivoSeleccionado === 'Dia de la Familia (Ley 1857)' && semestreSeleccionado === '2';
-  }
+
   getMinFecha() {
     const semestreSeleccionado = this.motivoForm.value.semestre;
     return semestreSeleccionado === '2' ? '2023-07-01' : '2023-01-01';
@@ -77,10 +79,11 @@ export class UserLicenciaPage implements OnInit {
   }
 
   async validarFechas() {
+
     const fechainicio = new Date(this.motivoForm.value.fechaInicio);
     const fechafin = new Date(this.motivoForm.value.fechaFin);
 
-    if (fechainicio > fechafin) {
+    if (fechainicio >= fechafin) {
       const alert = await this.alertController.create({
         header: "La fecha de inicio no puede ser mayor a la de fin",
         buttons: ["aceptar"],
@@ -93,12 +96,40 @@ export class UserLicenciaPage implements OnInit {
 
 
   FileChange(e: any) {
-    const files: File = this.motivoForm.value.constancia
-    console.log(files)
+    const archivos: FileList = e.files
 
+    if (archivos && archivos.length > 0) {
+      this.motivoForm.get('fechaInicio')?.enable()
+      this.motivoForm.get('fechaFin')?.enable()
+      for (let i = 0; i < archivos.length; i++) {
+        const archivo: File = archivos[i];
+        if (archivo.type === 'application/pdf' || archivo.type === 'image/png' || archivo.type === 'image/jpeg') {
+          this.documentos.push(archivo)
+
+          const reader = new FileReader();
+
+          console.log('2')
+
+          reader.onload = (ev: any) => {
+            console.log('3')
+            console.log(ev.result)
+            // this.miniaturas.push(e.result)
+          };
+          // const urlArchivo: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(archivo))
+          // console.log(urlArchivo)
+        } else {
+          console.log(archivo.name + ' archivo invalido')
+        }
+
+      }
+    } else {
+      this.motivoForm.get('fechaInicio')?.disable()
+      this.motivoForm.get('fechaFin')?.disable()
+    }
   }
 
   submitForm() {
-    console.log(this.motivoForm.value.constancia)
+    this.validarFechas();
+
   }
 }
